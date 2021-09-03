80c408f6be2fa3321c3adb15a22f24cfc0df398f
alvinsky <alvinsky@developer-pc>
2021-09-03 13:28:40 +0700

    heroku

--- panggilbridge/src/main/java/com/parakoder/panggilbridge/Controller.java
+++ panggilbridge/src/main/java/com/parakoder/panggilbridge/Controller.java
@@ -3,6 +3,8 @@ package com.parakoder.panggilbridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,10 +14,14 @@ import org.springframework.web.client.RestTemplate;
 
 import com.parakoder.panggilbridge.dao.AntrianPanggil;
 import com.parakoder.panggilbridge.model.GetPanggil;
-
+@PropertySource({
+    "file:src/main/resources/antrianbridge_application.properties" 
+})
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class Controller {
+	@Autowired
+    Environment env;
 	@Autowired
 	private AntrianPanggil antrianPanggil;
 	
@@ -74,13 +80,13 @@ public class Controller {
 			if(status>7) {
 				//online
 				RestTemplate restTemplate = new RestTemplate();
-				String fooResourceUrloffline = "http://localhost:8001/v1/api/antrian/call?idPelayanan="+(status-7);
+				String fooResourceUrloffline = env.getProperty("API_DASHBOARD")+"/v1/api/antrian/call?idPelayanan="+(status-7);
 				GetPanggil getPanggil = restTemplate.getForObject(fooResourceUrloffline, GetPanggil.class);
 				return getPanggil;
 			}else {
 				//offline
 				RestTemplate restTemplate = new RestTemplate();
-				String fooResourceUrloffline = "http://localhost:8001/v1/api/antrian/call-offline?idPelayanan="+status;
+				String fooResourceUrloffline = env.getProperty("API_DASHBOARD")+"/v1/api/antrian/call-offline?idPelayanan="+status;
 				GetPanggil getPanggil = restTemplate.getForObject(fooResourceUrloffline, GetPanggil.class);
 				return getPanggil;
 			}
